54056fbdbd9205a9bd86fd06a9f32cc4881977a9
NichiNect <yoniwidhi2@gmail.com>
2021-04-24 09:51:54 +0700

    data-test, get haar cascade algorithm

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import cv2
 
-print('anu')
+# Load some pre-trained data on face frontals from opencv (haar cascade algorithm)
+trainedFaceData =  cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+print('Hello Code!')
